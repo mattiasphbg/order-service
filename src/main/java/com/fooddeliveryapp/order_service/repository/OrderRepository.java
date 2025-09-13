0a0e3a7a54a2974db3e9d9ccc6542f75f24928d7
mattiasphbg <mattiasp@hotmail.com>
2025-09-13 13:29:23 +0200

    Implement OrderRepository interface: extend JpaRepository and add methods for querying orders by customer email, status, customer ID, total amount, and payment transaction ID.

--- src/main/java/com/fooddeliveryapp/order_service/repository/OrderRepository.java
+++ src/main/java/com/fooddeliveryapp/order_service/repository/OrderRepository.java
@@ -1,5 +1,22 @@
 package com.fooddeliveryapp.order_service.repository;
 
-public class OrderRepository {
-    
+import com.fooddeliveryapp.order_service.entity.Order;
+import com.fooddeliveryapp.order_service.entity.OrderStatus;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.math.BigDecimal;
+import java.util.List;
+import java.util.Optional;
+
+@Repository
+public interface OrderRepository extends JpaRepository<Order, Long> {
+
+      List<Order> findByCustomerEmail(String customerEmail);
+      List<Order> findByStatus(OrderStatus status);
+      List<Order> findByCustomerIdAndStatus(String customerId, OrderStatus status);
+      List<Order> findByTotalAmountGreaterThan(BigDecimal amount);
+      Optional<Order> findByPaymentTransactionId(String paymentTranstionID);
+      
+      
 }

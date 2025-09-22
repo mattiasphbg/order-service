e658075de3a65e9da8e00de73ad6325462f30b53
mattiasphbg <mattiasp@hotmail.com>
2025-09-22 17:29:00 +0200

    Add methods for retrieving, updating, and deleting orders in OrderService; enhance error handling with EntityNotFoundException and improve order mapping logic

--- src/main/java/com/fooddeliveryapp/order_service/service/OrderService.java
+++ src/main/java/com/fooddeliveryapp/order_service/service/OrderService.java
@@ -11,6 +11,7 @@ import com.fooddeliveryapp.order_service.kafka.OrderCreatedEventPublisher;
 import com.fooddeliveryapp.order_service.kafka.OrderPlacedEvent;
 import com.fooddeliveryapp.order_service.repository.OrderRepository;
 import jakarta.persistence.EntityExistsException;
+import jakarta.persistence.EntityNotFoundException;
 import lombok.extern.slf4j.Slf4j;
 
 import org.springframework.data.repository.query.Param;
@@ -63,4 +64,82 @@ public class OrderService {
                  .orElseThrow(() -> new EntityExistsException("Order not found with id:" + orderId));
         return mapToResponseDto(order);
     }
+
+    @Transactional(readOnly = true)
+    public List<OrderResponse> getAllOrders(){
+        return orderRepository.findAll().stream()
+                .map(this::mapToResponseDto)
+                .collect(Collectors.toList());
+    }
+
+
+    @Transactional
+    public OrderResponse updateOrder(Long orderId, OrderRequest orderRequest){
+        Order existingOrder = orderRepository.findById(orderId)
+                .orElseThrow(() -> new EntityNotFoundException("Order not found with id: " + orderId));
+        existingOrder.setCustomerId(orderRequest.getCustomerId());
+        existingOrder.setCustomerEmail(orderRequest.getCustomerEmail());
+        existingOrder.setPickupLocation(orderRequest.getPickupLocation());
+        existingOrder.setDropoffLocation(orderRequest.getDropoffLocation());
+        existingOrder.setSubtotal(orderRequest.getSubTotal());
+        existingOrder.setDeliveryFee(orderRequest.getDeliveryFee());
+        existingOrder.setTaxAmount(orderRequest.getTaxAmount());
+        existingOrder.setTipAmount(orderRequest.getTipAmount());
+        existingOrder.setTotalAmount(orderRequest.getTotalAmount());
+        existingOrder.setCurrency(orderRequest.getCurrency());
+
+        existingOrder.getItems().clear();
+        orderRequest.getItems().forEach(itemRequest -> {
+            OrderItem item = mapToEntity(itemRequest);
+            item.setOrder(existingOrder);
+            existingOrder.getItems().add(item);
+        });
+
+        Order updatedOrder = orderRepository.save(existingOrder);
+        log.info("Order updated in database with ID: {}", updatedOrder.getId());
+
+        return mapToResponseDto(updatedOrder);
+    }
+
+    @Transactional
+    public void deleteOrder(Long orderId){
+        if(!orderRepository.existsById(orderId)) {
+            throw new EntityNotFoundException("Order not found with ID:", + orderId);
+        }
+        orderRepository.deleteById(orderId);
+        log.info("Order deleted from database with ID: {}", orderId);
+    }
+
+    private Order mapToEntity(OrderRequest request){
+        Order order = Order.builder().customerId(request.getCustomerId())
+                .customerEmail(request.getCustomerEmail()).
+                .pickupLocation(request.getPickupLocation())
+                .dropoffLocation(request.getDropoffLocation()).
+                .subtotal(request.getSubTotal()).
+                .deliveryFee(request.getDeliveryFee()).
+                .taxAmount(request.getTaxAmount()).
+                .tipAmount(request.getTipAmount()).
+                .totalAmount(request.getTotalAmount()).
+                         add(request.getDeliveryFee()).
+                         add(Optional.ofNullable(request.getTaxAmount()).orElse(BigDecimal.ZERO)).
+                         add(Optional.ofNullable(request.getTipAmount()).orElse(BigDecimal.ZERO))
+                .currency(request.getCurrency())
+                .status(OrderStatus.PENDING).build();
+
+        if(request.getItems() != null) {
+            List<OrderItem> orderItems = request.getItems().stream()
+                      .map(itemRequest ->{
+
+                          OrderItem item = mapOrderItemRequestToEntity(request);
+                          item.setOrder(order);
+                          return item;
+                        }
+                           ).collect(Collectors.toList());
+            order.setItems(orderItems);
+        }
+        return order;
+        }
+                
+    }
+
 }
